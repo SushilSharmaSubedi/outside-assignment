c26079d719b739011ae94097d129003f2901ae68
SushilSharmaSubedi <sushil.subedi@outside.tech>
2024-11-05 12:56:39 +0545

    Create user auth system

---
+++ php-assignment/day-3/user_auth_system/app/config/dbconfig.php
@@ -0,0 +1,13 @@
+<?php
+$host = 'localhost';
+$dbname = 'user_auth_system';
+$username = 'root';
+$password = '';
+try{
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    echo "Connected to the database successfully";
+} catch(PDOException $e){
+    echo "Connection failed: " . $e->getMessage();
+}
+?>
