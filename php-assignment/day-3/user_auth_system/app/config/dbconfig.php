6f84a0f2e8be902236749dc775a5fead5868021f
SushilSharmaSubedi <sushil.subedi@outside.tech>
2024-11-06 12:39:23 +0545

    Feature :: Create a .env file and a .gitignore file

--- php-assignment/day-3/user_auth_system/app/config/dbconfig.php
+++ php-assignment/day-3/user_auth_system/app/config/dbconfig.php
@@ -1,8 +1,9 @@
 <?php
-$host = 'localhost';
-$dbname = 'user_auth_system';
-$username = 'root';
-$password = '';
+include __DIR__ . '/../../.env';
+$host = $host;
+$dbname = $dbname;
+$username = $username;
+$password = $password;
 try{
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
