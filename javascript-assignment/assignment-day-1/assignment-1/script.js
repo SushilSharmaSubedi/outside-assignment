68812cc6bd98271e2f999e0eb14a0567b57f7af4
SushilSharmaSubedi <sushil.subedi@outside.tech>
2024-11-14 15:26:00 +0545

    Feature :: Console the data with appropriate console tools

---
+++ javascript-assignment/assignment-day-1/assignment-1/script.js
@@ -0,0 +1,15 @@
+let array = [1, 2, 3, 4, 5];
+let object = {
+  firstName: "Sushil",
+  middleName: "Sharma",
+  lastName: "Subedi",
+  age: 25,
+  address: "Nakhipot",
+  phoneNumber: "9898989898",
+};
+
+console.log(array);
+console.log(object);
+
+console.table(array);
+console.table(object);
