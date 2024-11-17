60faa7421099679e8b0a3f5ee327ddda9b5fecec
SushilSharmaSubedi <sushil.subedi@outside.tech>
2024-11-17 13:30:06 +0545

    Difference between function and block scope

--- javascript-assignment/assignment-day-1/assignment-3/script.js
+++ javascript-assignment/assignment-day-1/assignment-3/script.js
@@ -1,13 +1,13 @@
 function functionScopeExample() {
   if (true) {
-    var functionScopedVar = "I am function scoped!";
+    var functionScopedVar = "I am function scope!";
   }
   console.log(functionScopedVar); // This will log: "I am function scoped!"
 }
 
 function blockScopeExample() {
   if (true) {
-    let blockScoped = "I am block scoped!";
+    let blockScoped = "I am block scope!";
     console.log(blockScoped);
   }
   // console.log(blockScopedVar); // Uncommenting this line will cause an error: ReferenceError
